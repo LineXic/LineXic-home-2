577be0e6bbcfa17fd864699ba997f76afbf754a0
LineXic <linexic@yeah.net>
2025-01-22 17:30:54 +0800

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+    <el-breadcrumb separator="/">
+    <el-breadcrumb-item :to="{ path: '/' }">主页</el-breadcrumb-item>
+    <el-breadcrumb-item>
+      <a href="https://linexic.top">博客</a>
+    </el-breadcrumb-item>
+    <el-breadcrumb-item>  
+    <a href="https://linexic.top">掘金</a>
+    </el-breadcrumb-item>
+    <el-breadcrumb-item>      
+    <a href="https://github.com/LineXic">Github</a>
+    </el-breadcrumb-item>
+  </el-breadcrumb> 
+  </template>
