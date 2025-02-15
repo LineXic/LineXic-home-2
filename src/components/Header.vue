bf3437eb84fdd822f06638086d1041e3f445d859
文心 <121679359+LineXic@users.noreply.github.com>
2025-02-16 00:29:58 +0800

    更新 Header.vue
    更改

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,7 @@
 <template>
     <div class="min-h-screen flex flex-col justify-center items-center">
+    <img class="w-24 h-24 rounded-full mx-auto" src="@/assets/avatar.webp"/>
+<br/>
     <p class="text-slate-500 hover:text-blue-600 text-center mb-4">一珊一珊亮晶晶</p>
       <el-breadcrumb separator="/" class="flex justify-center">
       <el-breadcrumb-item :to="{ path: '/' }">主页</el-breadcrumb-item>
