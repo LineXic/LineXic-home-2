ea607107e9657acef136ac973584e3d4c564d7af
文心 <121679359+LineXic@users.noreply.github.com>
2025-02-15 23:59:22 +0800

    更新 Header.vue
    提问ds改进

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,18 @@
 <template>
-    <p class="text-slate-500 hover:text-blue-600 text-center">一珊一珊亮晶晶</p><br/>
-    <el-breadcrumb style="display:flex;
-justify-content:center;" separator="/">
-    <el-breadcrumb-item :to="{ path: '/' }">主页</el-breadcrumb-item>
-    <el-breadcrumb-item>
-      <a href="https://linexic.top">博客</a>
-    </el-breadcrumb-item>
-    <el-breadcrumb-item>  
-    <a href="https://juejin.cn/user/917459279025964">掘金</a>
-    </el-breadcrumb-item>
-    <el-breadcrumb-item>      
-    <a href="https://github.com/LineXic">Github</a>
-    </el-breadcrumb-item>
-  </el-breadcrumb> 
-  </template>
+  <div>
+    <p class="text-slate-500 hover:text-blue-600 text-center mb-4">一珊一珊亮晶晶</p>
+    
+    <el-breadcrumb separator="/" class="flex justify-center">
+      <el-breadcrumb-item :to="{ path: '/' }">主页</el-breadcrumb-item>
+      <el-breadcrumb-item>
+        <a href="https://linexic.top" target="_blank" rel="noopener noreferrer">博客</a>
+      </el-breadcrumb-item>
+      <el-breadcrumb-item>
+        <a href="https://juejin.cn/user/917459279025964" target="_blank" rel="noopener noreferrer">掘金</a>
+      </el-breadcrumb-item>
+      <el-breadcrumb-item>
+        <a href="https://github.com/LineXic" target="_blank" rel="noopener noreferrer">Github</a>
+      </el-breadcrumb-item>
+    </el-breadcrumb>
+  </div>
+</template>
